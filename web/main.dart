d4ca3b2b138f82be7385bbae337b52ad6bb7ee55
Max Morris <maxmorris2001@gmail.com>
2018-09-10 12:08:53 -0500

    Got the map working. Google shows a warning b/c no API key yet

--- web/main.dart
+++ web/main.dart
@@ -1,5 +1,10 @@
 import 'dart:html';
+import 'package:google_maps/google_maps.dart';
 
 void main() {
-  querySelector('#output').text = 'Haha, hello Mark!';
+  querySelector('#output').text = 'Map above lol';
+  final mapOptions = new MapOptions()
+    ..zoom = 8
+    ..center = new LatLng(-34.397, 150.644);
+  new GMap(document.getElementById("map-canvas"), mapOptions);
 }
