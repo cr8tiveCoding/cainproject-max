7d91f6633ac50290c2284b6dbf6f7ae7158f4285
Max Morris <maxmorris2001@gmail.com>
2018-09-11 13:24:21 -0500

    added interactive stuff (lat/long/zoom)

--- web/main.dart
+++ web/main.dart
@@ -7,4 +7,15 @@ void main() {
     ..zoom = 8
     ..center = new LatLng(-34.397, 150.644);
   new GMap(document.getElementById("map-canvas"), mapOptions);
+
+  querySelector('#button').onClick.listen((event) {
+    double lat = double.parse((querySelector('#lat_input') as InputElement).value);
+    double long = double.parse((querySelector('#long_input') as InputElement).value);
+    double zoom = double.parse((querySelector('#zoom_input') as InputElement).value);
+
+    final mapOptions = new MapOptions()
+      ..zoom = zoom
+      ..center = new LatLng(lat, long);
+    new GMap(document.getElementById("map-canvas"), mapOptions);
+  });
 }
