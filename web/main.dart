f20625917b086ca3dad3235e213a7e440989258d
Max Morris <maxmorris2001@gmail.com>
2018-09-11 19:56:46 -0500

    separated code into some functions

--- web/main.dart
+++ web/main.dart
@@ -3,19 +3,32 @@ import 'package:google_maps/google_maps.dart';
 
 void main() {
   querySelector('#output').text = 'Map above lol';
+
   final mapOptions = new MapOptions()
     ..zoom = 8
     ..center = new LatLng(-34.397, 150.644);
   new GMap(document.getElementById("map-canvas"), mapOptions);
 
-  querySelector('#button').onClick.listen((event) {
-    double lat = double.parse((querySelector('#lat_input') as InputElement).value);
-    double long = double.parse((querySelector('#long_input') as InputElement).value);
-    double zoom = double.parse((querySelector('#zoom_input') as InputElement).value);
+  querySelector('#button').onClick.listen(showNewMap);
+}
 
-    final mapOptions = new MapOptions()
-      ..zoom = zoom
-      ..center = new LatLng(lat, long);
-    new GMap(document.getElementById("map-canvas"), mapOptions);
-  });
+void showNewMap(event) {
+  double lat  = double.parse(queryInput('#lat_input')),
+         long = double.parse(queryInput('#long_input')),
+         zoom = double.parse(queryInput('#zoom_input'));
+
+  final mapOptions = new MapOptions()
+    ..zoom = zoom
+    ..center = new LatLng(lat, long);
+
+  new GMap(document.getElementById("map-canvas"), mapOptions);
+}
+
+String queryInput(String str) {
+  Element element = querySelector(str);
+
+  if (element != null && element is InputElement)
+    return element.value;
+
+  throw AssertionError('Queried element ${element} must be non-null InputElement');
 }
